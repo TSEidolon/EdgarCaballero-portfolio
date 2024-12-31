eb8c38ffe610cec37a9abfd0be4a2708599642d4
Edgar Francis Caballero <efrcaballero@gmail.com>
2024-12-30 18:26:20 -0700

    tested camera controls

--- src/components/heroSection.jsx
+++ src/components/heroSection.jsx
@@ -1,10 +1,29 @@
+import {Canvas} from '@react-three/fiber'
+import {CameraControls} from '@react-three/drei'
 
 
 const HeroSection = () => {
   return (
-    <div className="min-h-screen bg-[color:var(--primary-color)] py-10">
-      hello from hero
-    </div>
+    <section className='min-h-screen bg-[color:var(--primary-color)] py-10'>
+      <section className='top-hero'>
+        <div className="">
+          hello from hero
+        </div>
+      </section>
+      <section className='bottom-hero w-full h-full inset-0 absolute'>
+        <Canvas >
+          <mesh >
+            <boxGeometry args={[2, 2, 2]}/>
+            <meshStandardMaterial color='red'/>
+            <ambientLight intensity={1}/>
+            {/* <directionalLight color='red' position={[1, 1, 1]} /> */}
+            <CameraControls />
+          </mesh>
+        </Canvas>
+      </section>
+
+    </section>
+
   )
 }
 
