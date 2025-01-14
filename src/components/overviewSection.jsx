b0dfe6aa93c7e00b6f5ba9949121e632aabae051
Edgar Francis Caballero <efrcaballero@gmail.com>
2025-01-13 23:07:39 -0700

    added a 3d model and its animations on overviewSections

--- src/components/overviewSection.jsx
+++ src/components/overviewSection.jsx
@@ -1,10 +1,27 @@
+import {Canvas} from '@react-three/fiber'
+import { PerspectiveCamera} from '@react-three/drei'
+import { Suspense } from 'react'
+import { Parallax } from 'react-scroll-parallax';
+import CanvasLoader from './canvasLoader'
 
+import { BirdsOverview } from './birdsOverview';
 
 const OverviewSection = () => {
   return (
-    <div className='h-screen bg-blue-300 border-2 border-blue-500'>
+    <div className='h-screen bg-blue-300 border-2 border-blue-500 relative'>
 
       OverviewSection
+
+      <section className='w-full h-full inset-0 absolute'>
+      <Canvas className='' >
+          <Suspense fallback={<CanvasLoader/>}>
+            <PerspectiveCamera  position={[0,0,30]} />
+            <BirdsOverview   scale={1.2} rotation={[0,1,0]}/>
+            <ambientLight intensity={1}/>
+            <directionalLight position={[10,10,10]} intensity={0.5}/>
+          </Suspense>
+        </Canvas>
+      </section>
     </div>
   )
 }
