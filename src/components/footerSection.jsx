e3576d0e0a8d443e26fc55841ed111d1783afab1
Edgar Francis Caballero <efrcaballero@gmail.com>
2025-01-28 08:27:04 -0700

    testing text placement on model

--- src/components/footerSection.jsx
+++ src/components/footerSection.jsx
@@ -1,28 +1,38 @@
-import React from 'react'
-import {Canvas} from '@react-three/fiber'
-import { PerspectiveCamera, OrbitControls} from '@react-three/drei'
-import { Suspense } from 'react'
-import CanvasLoader from './canvasLoader'
+import {useState, useEffect, Suspense} from 'react';
+import {Canvas} from '@react-three/fiber';
+import { PerspectiveCamera, OrbitControls, Text} from '@react-three/drei';
+import CanvasLoader from './canvasLoader';
 import { BeachFooter } from './beachFooter';
 
 
 
 const FooterSection = () => {
+  const [hovered, SetHovered] = useState(false)
+  const over = () => SetHovered(true)
+  const out = () => SetHovered(false)
+
+  useEffect(() =>{
+    if (hovered) document.body.style.cursor = 'pointer'
+    return () => (document.body.style.cursor = 'auto')
+  }, [hovered])
+
   return (
     <div className='h-screen border-2 border-black relative'>
       <section className='right-area-section'>
 
       </section>
-      <section className='right-area-section w-full h-full inset-0 absolute z-0'>
-        <Canvas>
+      <section className='right-area-section w-full h-full inset-0 absolute z-0 '>
+
+        <Canvas className=''>
           <Suspense fallback={CanvasLoader}>
-            {/* <OrbitControls /> */}
-            <BeachFooter scale={.5} position={[1, -.5, 0]} rotation={[.2,1,0]}/>
+            {/* <OrbitControls  /> */}
+            <Text color="black" anchorX="right" anchorY="middle" onPointerOver={over} onPointerOut={out} onClick={() => console.log('clicked')}>
+              hello world!
+            </Text>
+            <BeachFooter scale={.5} />
           </Suspense>
-
-
-
         </Canvas>
+
       </section>
     </div>
   )
