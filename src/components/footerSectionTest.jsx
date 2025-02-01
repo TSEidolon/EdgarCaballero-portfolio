60ba96231b5423d787781f9dedecbb16ecb03bb9
Edgar Francis Caballero <efrcaballero@gmail.com>
2025-02-01 01:15:29 -0700

    went another route with the animations and opted for individual animation files instead

--- src/components/footerSectionTest.jsx
+++ src/components/footerSectionTest.jsx
@@ -6,25 +6,28 @@ import { GeckoFooterTest } from './geckoFooterTest';
 
 
 const FooterSectionTest = () => {
-
+  const [animationName, setAnimationName] = useState('idle')
 
   return (
-    <div className='h-screen border-2 border-black relative'>
+    <div className='h-screen border-2 border-black relative flex justify-center items-center'>
       <section className='Left-area-section p-10'>
-        <div className='p-10  w-[400px] h-[400px] z-10 border-2 border-green-500'>
-          <p >
+        <div className='p-10  w-[400px] h-[400px] z-0 border-2 border-green-500'>
+          <p      onClick={() => setAnimationName("bounce")}
+                  onPointerOver={() => setAnimationName("bounce")}
+                  onPointerOut={() => setAnimationName('idle')}
+          >
             Test Area
           </p>
         </div>
       </section>
-      <section className='right-area-section w-full h-full inset-0 absolute z-0 '>
+      <section className='right-area-section w-[60%] h-full z-0 '>
 
         <Canvas className=''>
           <Suspense fallback={CanvasLoader}>
             <OrbitControls enableZoom={false} />
             <ambientLight intensity={1.2}/>
             <directionalLight position={[0,2,0]} intensity={1}/>
-            <GeckoFooterTest scale={2.5}/>
+            <GeckoFooterTest scale={2.5} animationName={animationName}/>
           </Suspense>
         </Canvas>
 
