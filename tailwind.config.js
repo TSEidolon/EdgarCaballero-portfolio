1cb7c0bdc7ef6c92284539ea36bec3bea932eb18
Edgar Francis Caballero <efrcaballero@gmail.com>
2025-02-06 18:04:34 -0700

    edited file path for background images using the roots absolute path so it appears on the build output

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,12 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        'hero-background':"url('src/assets/backgroundBricks.png')",
-        'hero-background-two':"url('src/assets/leavesBanner.png')",
-        'hero-background-test':"url('src/assets/leavesBannerOne.png')",
-        'overview-background':"url('src/assets/leavesShadow.png')",
-        'comic-background':"url('src/assets/comicDots.png')",
-        'projects-background':"url('src/assets/plantProjects.png')",
+        'hero-background':"url('/src/assets/backgroundBricks.png')",
+        'hero-background-two':"url('/src/assets/leavesBanner.png')",
+        'hero-background-test':"url('/src/assets/leavesBannerOne.png')",
+        'overview-background':"url('/src/assets/leavesShadow.png')",
+        'comic-background':"url('/src/assets/comicDots.png')",
+        'projects-background':"url('/src/assets/plantProjects.png')",
 
       }
     },
